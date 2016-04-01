8f14e67e9c9b6e61c28409a30e8a87b64f8dca5a
Parminder Klair <p.klair@scorchsoft.com>
2016-04-01 16:51:30 +0100

    ui link fix

--- components/UiLink/UiLink.js
+++ components/UiLink/UiLink.js
@@ -1,22 +1,39 @@
 import React, { Component, PropTypes } from 'react';
 import _ from 'underscore';
 import simpleStorage from 'simplestorage.js';
+import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
 
 class UiLink extends Component {
   getLink() {
     let value = simpleStorage.get('ref-key');
 
-    if (_.isUndefined(value)) {
+    if (this.getQueryStringValue('ref') !== null) {
+      value = this.getQueryStringValue('ref');
+      return `http://ui.stackable.space/login?ref=${value}`;
+    } else if (_.isUndefined(value)) {
       return 'http://ui.stackable.space';
     } else {
       return `http://ui.stackable.space/login?ref=${value}`;
     }
   }
 
+  getQueryStringValue (name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
+      results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+
   render() {
     const { children, ...props } = this.props;
-
-    return <a {...props} href={this.getLink()}>{children}</a>;
+    if (canUseDOM) {
+      return <a {...props} href={this.getLink()}>{children}</a>;
+    } else {
+      return <a {...props} href="http://ui.stackable.space">{children}</a>;
+    }
   }
 }
 
